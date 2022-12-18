7afbb28bf25e6814f97df2d12d8cfa53e8ea1d84
Valeri_L <idval@imedia.ru>
2022-12-18 07:33:07 +0300

    ДЗ по 1-му семинару

---
+++ Sem001_002/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает (целое) число
+//       и выдает, является ли число четным (делится ли оно на два без остатка)
+//
+using static MyLibrary;   //  ссылка на библиотеку
+//
+// используем написанный ранее методы 
+//  - DisplayReplyInt() - ввод целого числа с терминала
+//  - EvenOdd(a) - определение четности числа
+int a;
+string End = "Да";
+
+while (End != "N")
+{
+a = DisplayReplyInt("Введите целое число");
+
+
+if (EvenOdd(a)) Console.WriteLine($"{a,3}" + " является четным");
+else Console.WriteLine($"{a,3}" + " является НЕчетным");
+Console.WriteLine("Продолжить проверку (Y/N");
+End = Console.ReadLine(); 
+if (End == "n") break;
+if (End == "т") break;
+if (End == "Т") break;
+
+}
+
+
